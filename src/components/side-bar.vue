d3be008ab56c132a6acf0ad4a6d1d25f7559e8ff
bastarder <jie.qian@blockheaders.com>
2019-03-08 10:53:46 +0800

    step 4

--- src/components/side-bar.vue
+++ src/components/side-bar.vue
@@ -21,42 +21,74 @@
         no-border
         link
       >
-        <q-collapsible icon="explore" label="Eduaction" collapse-icon="expand_more">
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="Dashboard v.1"/>
-          </q-item>
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="Dashboard v.2"/>
-          </q-item>
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="Dashboard v.3"/>
-          </q-item>
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="Analytics"/>
-          </q-item>
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="widgets"/>
-          </q-item>
-        </q-collapsible>
+        <div>
+          <div>
+            <q-popover
+              v-model="miniStatus.eduaction"
+              anchor="top right"
+              self="top left"
+              >
+              <div>12312312312</div>
+              <div>12312312312</div>
+              <div>12312312312</div>
+              <div>12312312312</div>
+              <div>12312312312</div>
+            </q-popover>
+          </div>
+          <q-collapsible icon="explore" label="Eduaction" collapse-icon="expand_more" @click.native="openList('eduaction')">
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="Dashboard v.1"/>
+            </q-item>
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="Dashboard v.2"/>
+            </q-item>
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="Dashboard v.3"/>
+            </q-item>
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="Analytics"/>
+            </q-item>
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="widgets"/>
+            </q-item>
+          </q-collapsible>
+        </div>
+
+
         <q-item @click.native="openURL('http://quasar-framework.org')">
           <q-item-side icon="school" />
           <q-item-main label="Event"/>
         </q-item>
 
-        <q-collapsible icon="explore" label="Professor" collapse-icon="expand_more">
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="All Professors"/>
-          </q-item>
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="Add Professor"/>
-          </q-item>
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="Edit Professor"/>
-          </q-item>
-          <q-item @click.native="openURL('http://quasar-framework.org')">
-            <q-item-main label="Professor Profile"/>
-          </q-item>
-        </q-collapsible>
+        <div>
+          <q-collapsible icon="explore" label="Professor" collapse-icon="expand_more" @click.native="openList('professor')">
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="All Professors"/>
+            </q-item>
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="Add Professor"/>
+            </q-item>
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="Edit Professor"/>
+            </q-item>
+            <q-item @click.native="openURL('http://quasar-framework.org')">
+              <q-item-main label="Professor Profile"/>
+            </q-item>
+          </q-collapsible>
+
+          <q-popover
+            class="nav-bar"
+            anchor="top right"
+            self="top left"
+            >
+            <div>12312312312</div>
+            <div>12312312312</div>
+            <div>12312312312</div>
+            <div>12312312312</div>
+            <div>12312312312</div>
+          </q-popover>
+
+        </div>
 
         <q-collapsible icon="explore" label="Students" collapse-icon="expand_more">
           <q-item @click.native="openURL('http://quasar-framework.org')">
@@ -84,10 +116,34 @@ import { openURL } from 'quasar'
 export default {
   props: ['leftDrawerMini'],
   data () {
-    return {}
+    return {
+      miniStatus: {
+        eduaction: false,
+        professor: false,
+      },
+      count: {
+        eduaction: 0,
+        professor: 0,
+      },
+    }
   },
   methods: {
     openURL,
+    openList: function(key) {
+      this.count[key]++;
+
+      if(this.count[key] % 2 === 0){
+        return;
+      }
+
+      for (const _key in this.miniStatus) {
+        if(!this.leftDrawerMini){
+          this.miniStatus[_key] = false;
+          continue;
+        }
+        this.miniStatus[_key] = _key === key
+      }
+    }
   }
 }
 </script>
